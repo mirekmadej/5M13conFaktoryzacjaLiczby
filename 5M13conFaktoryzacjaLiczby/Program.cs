121717de5b5f548e308598ca2f1c0773fecb114e
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-11-20 12:43:07 +0100

    Dodaj pliki projektów.

---
+++ 5M13conFaktoryzacjaLiczby/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _5M13conFaktoryzacjaLiczby
+{
+    /*
+     * Faktoryzacja liczby
+     * wczytaj liczne naturalną n i wypisz wszystkie dzielniki
+     * 
+     * przukład:
+     * 60 = 2*2*3*5
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("podaj liczbę: ");
+            int n = int.Parse(Console.ReadLine());
+            int dzielnik = 2;
+            while(n > 1)
+            {
+                while(n % dzielnik == 0)
+                {
+                    Console.WriteLine(dzielnik);
+                    n = n / dzielnik;
+                }
+                dzielnik++;
+            }
+        }
+    }
+}
